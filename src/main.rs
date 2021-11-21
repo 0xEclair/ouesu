119b5954423d333f5badacb148942890aa712acb
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2021-11-22 02:55:26 +0800

    :feat a freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, ouesu!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // bootloader
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
