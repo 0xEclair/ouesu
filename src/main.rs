a4d5fa1b8518fff1882a1d17ad9fc6a1b4bc82ae
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-04-09 02:52:46 +0800

    :feat translate physical address

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(ouesu::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
+use core::mem::transmute;
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 
@@ -15,20 +17,24 @@ entry_point!(kernel_main);
 
 #[cfg(not(test))]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use ouesu::memory::active_level_4_table;
+    use ouesu::memory;
     use x86_64::VirtAddr;
+    use x86_64::structures::paging::{
+        PageTable,
+        Translate
+    };
+
     println!("Hello ouesu{}", "!");
     ouesu::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe {
-        active_level_4_table(phys_mem_offset)
-    };
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let addresses = [0xb8000, 0x201008, 0x0100_0020_1a10, boot_info.physical_memory_offset];
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     println!("it didn't crash.");
